747a2717d3c39b2504d662187b09ae870f4bb4e1
KRISHNA-PRAVESH <krishnapravesh.hj@gmail.com>
2022-11-08 20:00:02 +0530

    added util functions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('heyy');
+//creating a map that stores name and their mobile number
+const contact = new Map();
+contact.set('krishna',9344930703);
+
+function add(){
+    let name = prompt('Enter name');
+    let number = prompt('Enter mobile number');
+    contact.set(name,number);
+}
+
+function search(){
+    let name = document.getElementById('name').value;
+
+    let num = contact.get(name);
+    if(num==undefined) alert('Not found');
+    else alert(`Name: ${name}, Number: ${contact.get(name)}`);
+    
+    
+}
