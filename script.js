8f82db9762ff01a7cc5a5f4ce6a66e4da9240858
KRISHNA-PRAVESH <krishnapravesh.hj@gmail.com>
2023-01-26 20:55:23 +0530

    modified script

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-console.clear()
+//Add contacts into the map also into the suggestions array
+const contacts = new Map();
+function addContact(){
+	let name = prompt("Enter name");
+	let number = prompt("Enter Number");
+	//Saving contact to the map.
+	contacts.set(name,number);
+
+	//adding contact name to the suggestions
+	contactNames.push(name);
+}
 
 //Picking elements in the HTML
-const inputContainerEl = document.querySelector(".input-container");
-const textInputEl = document.querySelector("input#text");
-const suggestionEl = document.querySelector(".suggestion-container");
+const inputContainer = document.querySelector(".input-container");
+const textInput = document.querySelector("input#text");
+const suggestion = document.querySelector(".suggestion-container");
 
 
 //Ascii values
@@ -15,79 +25,75 @@ const DOWN_ARROW_KEYCODE = 40;
 const SPACE_KEYCODE = 32;
 
 
-
 //contains all the contact names
-let wordsArray = [];
-
+let contactNames = [];
 let suggestedWord = "";
 let suggestedWordsArray = [];
 let currentWordIndex = 0;
 let insertText = false;
 
 
-textInputEl.addEventListener("input", e => {
+textInput.addEventListener("input", e => {
 	if (e.data != " ") {
 		insertText = true;
 	}
 	if (insertText == false) {
-		textInputEl.value = "";
+		textInput.value = "";
 	}
 
 	let inputValue = e.target.value;
-	suggestedWordsArray = filterArray(wordsArray, inputValue);
+	suggestedWordsArray = filterArray(contactNames, inputValue);
 	suggestedWord = suggestedWordsArray[0];
 
 	if (suggestedWord != undefined) {
-		suggestionEl.innerHTML = suggestedWord;
+		suggestion.innerHTML = suggestedWord;
 	}
 
 	if (inputValue.length == 0 || suggestedWordsArray.length == 0) {
-		suggestionEl.innerHTML = "";
+		suggestion.innerHTML = "";
 	}
  
 
-	if (textInputEl.value.length == 0) {
+	if (textInput.value.length == 0) {
 		insertText = false;
 	}
 });
 
 //When the user started entering a character
-textInputEl.addEventListener("keydown", e => {
+textInput.addEventListener("keydown", e => {
 
-    //if the user presses ENTER then all the typed character will erased
+    //if the user presses ENTER then all the typed character will be erased
 	if (e.keyCode == ENTER_KEYCODE) {
-		if (textInputEl.value.length == 0) return;
-		let inputValue = textInputEl.value;
+		if (textInput.value.length == 0) return;
+		let inputValue = textInput.value;
 		let words = inputValue.split(" ");
 		for (let i in words) {
 			if (words[i].length != 0) {
-				wordsArray.push(words[i]);
-				textInputEl.value = "";
-				suggestionEl.innerHTML = "";
+				contactNames.push(words[i]);
+				textInput.value = "";
+				suggestion.innerHTML = "";
 			}
 		}
-		wordsArray = removeDuplicatesFromArray(wordsArray);
-		inputContainerEl.classList.add("animate");
-		svgTabIcon.classList.add("hidden");
-		svgEnterIcon.classList.add("hidden");
-		removeClassAfterAnimationCompletes(inputContainerEl, "animate");
+		contactNames = removeDuplicatesFromArray(contactNames);
+
 	}
 
     
-	if (textInputEl.value.length != 0) {
+	if (textInput.value.length != 0) {
 
-        //If Arrow-up is pressed then go up in the lis of all related words
+        //If Arrow-up is pressed then go up in the list of all related words
 		if (e.keyCode == UP_ARROW_KEYCODE) {
 			if (currentWordIndex == 0) return;
 			currentWordIndex--;
-			suggestionEl.innerHTML = suggestedWordsArray[currentWordIndex];
+			//suggesting a word.
+			suggestion.innerHTML = suggestedWordsArray[currentWordIndex];
 		}
  
         //If arrow-down is pressed go down in the list of all related words
 		if (e.keyCode == DOWN_ARROW_KEYCODE) {
 			if (currentWordIndex == suggestedWordsArray.length - 1) return;
 			currentWordIndex++;
-			suggestionEl.innerHTML = suggestedWordsArray[currentWordIndex];
+			suggestion.innerHTML = suggestedWordsArray[currentWordIndex];
 		}
         
 
@@ -101,22 +107,14 @@ textInputEl.addEventListener("keydown", e => {
 		//if the user presses tab then fill the box with the currently suggested word
         if (e.keyCode == TAB_KEYCODE) {
 			e.preventDefault();
-			textInputEl.value = suggestedWordsArray[currentWordIndex];
-			suggestionEl.innerHTML = "";
-			svgTabIcon.classList.add("hidden");
-			svgEnterIcon.classList.add("hidden");
+			textInput.value = suggestedWordsArray[currentWordIndex];
+			suggestion.innerHTML = "";
 		}
 	}
 });
 
-removeClassAfterAnimationCompletes(inputContainerEl, "animate");
 
-function removeClassAfterAnimationCompletes(el, className) {
-	let elStyles = window.getComputedStyle(inputContainerEl);
-	setTimeout(function() {
-		el.classList.remove(className);
-	}, +elStyles.animationDuration.replace("s", "") * 1000);
-}
+
 
 
 //Filtering suggestions based on input 
@@ -143,26 +141,13 @@ function compareTwoStrings(string, subString) {
 
 
 
-//Add contacts into the map also into the suggestions array
-const contacts = new Map();
-function addContact(){
-    let name = prompt("Enter name");
-    let number = prompt("Enter Number");
-    contacts.set(name,number);
-
-    //adding name to the suggestions
-    wordsArray.push(name);
-
-    console.log(wordsArray)
-}
 
 //Printing the number of selected person if exists
 function Get(){
     let toGet = document.getElementById("text").value;
-
     let num = contacts.get(toGet);
     if(num!=undefined){
-        alert(`Name:${toGet} , Number: ${contacts.get(toGet)}`);
+        alert(`Name:${toGet} , Number: ${num}`);
 
     }
     else{
@@ -171,6 +156,6 @@ function Get(){
 }
 
 
-//Feature to be added: Determine who is callling Given their phone number
+//Feature to be added: Determine who is calling Given their phone number
 
 
